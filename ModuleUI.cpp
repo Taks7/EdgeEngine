5c92e4b20cf4d889615382e239bc14a3b57123be
Alejandromo125 <72094186+Alejandromo125@users.noreply.github.com>
2022-09-21 11:01:32 +0200

    Trying to start with ImGui

--- ModuleUI.cpp
+++ ModuleUI.cpp
@@ -5,6 +5,8 @@
 #include "ModuleUI.h"
 #include "ImGui/imgui.h"
 #include "ImGui/imgui_impl_glfw.h"
+#include "ImGui/imgui_impl_sdl.h"
+#include "Imgui/imgui_impl_opengl3.h"
 #include <glew.h>
 #include <glfw3.h>
 
@@ -22,8 +24,13 @@ ModuleUI::~ModuleUI()
 bool ModuleUI::Start()
 {
 	LOG("Loading start UI")
+	IMGUI_CHECKVERSION();
 
-
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	ImGui::StyleColorsDark();
+	ImGui_ImplSDL2_InitForOpenGL(App->window->window, App->renderer3D->context);
+	ImGui_ImplOpenGL3_Init();
 	return true;
 }
 update_status ModuleUI::Update(float dt)
