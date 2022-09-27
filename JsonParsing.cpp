42e913999f8d63a7fdbf2806637f71c226f25b8e
Taks7 <albramisaplay@gmail.com>
2022-09-27 14:26:49 +0200

    Added Json files

---
+++ JsonParsing.cpp
@@ -0,0 +1,216 @@
+#include "Globals.h"
+#include "JsonParsing.h"
+
+JsonParsing::JsonParsing()
+{
+	rootObject = json_value_init_object();
+}
+
+JsonParsing::JsonParsing(const char* string)
+{
+	rootObject = nullptr;
+	if (string != nullptr)
+	{
+		rootObject = json_parse_string(string);
+	}
+}
+
+JsonParsing::JsonParsing(JSON_Value* value)
+{
+	rootObject = value;
+}
+
+JsonParsing::~JsonParsing()
+{
+}
+
+size_t JsonParsing::Save(char** buf)
+{
+	size_t written = json_serialization_size(rootObject);
+	*buf = new char[written];
+	json_serialize_to_buffer(rootObject, *buf, written);
+	return written;
+}
+
+size_t JsonParsing::SaveFile(const char* name)
+{
+	size_t written = json_serialization_size(rootObject);
+	json_serialize_to_file(rootObject, name);
+	return written;
+}
+
+JSON_Value* JsonParsing::InitObject()
+{
+	JSON_Value* object = json_value_init_object();
+	rootObject = object;
+
+	return object;
+}
+
+
+JSON_Object* JsonParsing::SetNewJsonNode(JSON_Object* parent, const char* nodeName) const
+{
+	json_object_set_value(parent, nodeName, json_value_init_object());;
+
+	return json_object_get_object(ValueToObject(rootObject), nodeName);
+}
+
+JSON_Status JsonParsing::SetNewJsonString(JSON_Object* node, const char* name, const char* string) const
+{
+	return json_object_set_string(node, name, string);
+}
+
+JSON_Status JsonParsing::SetNewJsonNumber(JSON_Object* node, const char* name, double number) const
+{
+	return json_object_set_number(node, name, number);
+}
+
+JSON_Status JsonParsing::SetNewJson3Number(JsonParsing& node, const char* name, float3 number) const
+{
+	JSON_Array* array = SetNewJsonArray(node.GetRootValue(), name);
+
+	json_array_append_number(array, number.x);
+	json_array_append_number(array, number.y);
+	
+	return json_array_append_number(array, number.z);
+}
+
+JSON_Status JsonParsing::SetNewJson4Number(JsonParsing& node, const char* name, Quat number) const
+{
+	JSON_Array* array = SetNewJsonArray(node.GetRootValue(), name);
+
+	json_array_append_number(array, number.x);
+	json_array_append_number(array, number.y);
+	json_array_append_number(array, number.z);
+
+	return json_array_append_number(array, number.w);
+}
+
+JSON_Status JsonParsing::SetNewJsonBool(JSON_Object* node, const char* name, bool boolean) const
+{
+	return json_object_set_boolean(node, name, boolean);
+}
+
+JSON_Array* JsonParsing::SetNewJsonArray(JSON_Value* parent, const char* name) const
+{
+	json_object_dotset_value(ValueToObject(parent), name, json_value_init_array());
+
+	return json_object_dotget_array(ValueToObject(parent), name);
+}
+
+JSON_Status JsonParsing::SetValueToArray(JSON_Array* jsonArray, JSON_Value* value)
+{
+	return json_array_append_value(jsonArray, value);
+}
+
+JsonParsing JsonParsing::SetChild(JSON_Value* parent, const char* name)
+{
+	json_object_set_value(ValueToObject(parent), name, json_value_init_object());
+
+	return GetChild(parent, name);
+}
+
+size_t JsonParsing::ParseFile(const char* fileName)
+{
+	JSON_Value* config = json_parse_file(fileName);
+	rootObject = config;
+	
+	return json_serialization_size(rootObject);
+}
+
+JSON_Object* JsonParsing::ValueToObject(JSON_Value* value) const
+{
+	JSON_Object* object = json_value_get_object(value);
+	
+	if(object != NULL)
+		return object;
+
+	/*LOG("Object has not been retrieved");*/
+
+	return nullptr;
+}
+
+JSON_Status JsonParsing::SerializeFile(JSON_Value* value, const char* fileName) const
+{
+	return json_serialize_to_file_pretty(value, fileName);
+}
+
+JSON_Value* JsonParsing::GetRootValue() const
+{
+	return rootObject;
+}
+
+const char* JsonParsing::GetJsonString(const char* name) const
+{
+	return json_object_get_string(ValueToObject(rootObject), name);
+}
+
+double JsonParsing::GetJsonNumber(const char* name) const
+{
+	return json_object_get_number(ValueToObject(rootObject), name);
+}
+
+bool JsonParsing::GetJsonBool(const char* name) const
+{
+	return json_object_get_boolean(ValueToObject(rootObject), name);
+}
+
+JsonParsing JsonParsing::GetChild(JSON_Value* parent, const char* name)
+{
+	JsonParsing child; 
+	
+	child.rootObject = json_object_get_value(ValueToObject(rootObject), name);
+	
+	return child;
+}
+
+JsonParsing JsonParsing::GetJsonArrayValue(JSON_Array* array, int index) const
+{
+	return JsonParsing(json_array_get_value(array, index));
+}
+
+//Posible error de mathgeo !
+
+//float3 JsonParsing::GetJson3Number(JsonParsing& node, const char* name)
+//{
+//	JSON_Array* array = node.GetJsonArray(node.ValueToObject(node.GetRootValue()), name);
+//
+//	float x = json_array_get_number(array, 0);
+//	float y = json_array_get_number(array, 1);
+//	float z = json_array_get_number(array, 2);
+//
+//	return float3(x,y,z);
+//}
+
+//float4 JsonParsing::GetJson4Number(JsonParsing& node, const char* name)
+//{
+//	JSON_Array* array = node.GetJsonArray(node.ValueToObject(node.GetRootValue()), name);
+//
+//	float x = json_array_get_number(array, 0);
+//	float y = json_array_get_number(array, 1);
+//	float z = json_array_get_number(array, 2);
+//	float w = json_array_get_number(array, 3);
+//
+//	return float4(x, y, z, w);
+//}
+
+JSON_Object* JsonParsing::GetJsonObject(JSON_Object* parentObject, const char* node) const
+{
+	JSON_Object* object = json_object_get_object(parentObject, node);
+
+	if (object) return object;
+
+	/*LOG("Node object has not been found");*/
+
+	return nullptr;
+}
+
+JSON_Array* JsonParsing::GetJsonArray(JSON_Object* parentObject, const char* node) const
+{
+	return json_object_dotget_array(parentObject, node);
+}
+
+size_t JsonParsing::GetJsonArrayCount(JSON_Array* array) const
+{
+	return json_array_get_count(array);
+}
---
+++ JsonParsing.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "json/parson.h"
+#include "MathGeo/src/Math/float3.h"
+#include "MathGeo/src/Math/float4.h"
+#include "MathGeo/src/Math/Quat.h"
+
+class JsonParsing
+{
+public:
+	JsonParsing();
+	JsonParsing(const char* string);
+	JsonParsing(JSON_Value* value);
+	~JsonParsing();
+
+	size_t Save(char** buf);
+	size_t SaveFile(const char* name);
+
+	JSON_Object* SetNewJsonNode(JSON_Object* parent, const char* nodeName) const;
+	JSON_Status SetNewJsonString(JSON_Object* node, const char* name, const char* string) const;
+	JSON_Status SetNewJsonNumber(JSON_Object* node, const char* name, double number) const;
+	JSON_Status SetNewJson3Number(JsonParsing& node, const char* name, float3 number) const;
+	JSON_Status SetNewJson4Number(JsonParsing& node, const char* name, Quat number) const;
+	JSON_Status SetNewJsonBool(JSON_Object* node, const char* name, bool boolean) const;
+	JSON_Array* SetNewJsonArray(JSON_Value* parent, const char* name) const;
+	JSON_Status SetValueToArray(JSON_Array* jsonArray, JSON_Value* value);
+	JsonParsing SetChild(JSON_Value* parent, const char* name);
+
+	JSON_Object* GetJsonObject(JSON_Object* parentObject, const char* node) const;
+	JSON_Array* GetJsonArray(JSON_Object* parentObject, const char* node) const;
+	size_t GetJsonArrayCount(JSON_Array* array) const;
+	JSON_Value* GetRootValue() const;
+	const char* GetJsonString(const char* name) const;
+	double GetJsonNumber(const char* name) const;
+	bool GetJsonBool(const char* name) const;
+	JsonParsing GetChild(JSON_Value* parent, const char* name);
+
+	JsonParsing GetJsonArrayValue(JSON_Array* array, int index) const;
+	float3 GetJson3Number(JsonParsing& node, const char* name);
+	float4 GetJson4Number(JsonParsing& node, const char* name);
+	//GetValor("Width")
+
+	size_t ParseFile(const char* fileName);
+	JSON_Object* ValueToObject(JSON_Value* value) const;
+	JSON_Status SerializeFile(JSON_Value* value, const char* fileName) const;
+
+	JSON_Value* InitObject();
+private:
+
+	JSON_Value* rootObject;
+
+};
