13f3a81be1ba0402954c40b3369b3ea3d83ab3af
Alejandromo125 <72094186+Alejandromo125@users.noreply.github.com>
2022-09-28 09:12:05 +0200

    Update ModuleUI.h

--- ModuleUI.h
+++ ModuleUI.h
@@ -29,10 +29,10 @@ public:
 public:
 	//MENU THINGYS
 	void MainMenu();
-	bool fullscreen;
-	bool Vsync;
-	int screenWidth;
-	int screenHeight;
+	bool fullscreen = false;
+	bool Vsync = false;
+	int screenWidth = 1920;
+	int screenHeight = 1080;
 	
 	bool enableAboutMenu;
 
