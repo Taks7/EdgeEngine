8d880fad1c35f61a3f5f877ab6e30a14240f1ca0
Taks7 <albramisaplay@gmail.com>
2023-01-12 21:20:00 +0100

    Particle Emitters Completed

--- ParticlesEmitter.cpp
+++ ParticlesEmitter.cpp
@@ -1,19 +1,19 @@
-//#include "ParticlesEmitter.h"
-//
-//ParticleEmitter::ParticleEmitter()
-//{
-//	particleTexture = nullptr;
-//}
-//
-//void ParticleEmitter::Save()
-//{
-//
-//}
-//
-//void ParticleEmitter::Load()
-//{
-//
-//}
+#include "ParticlesEmitter.h"
+
+ParticleEmitter::ParticleEmitter()
+{
+	particleTexture = nullptr;
+}
+
+void ParticleEmitter::Save()
+{
+
+}
+
+void ParticleEmitter::Load()
+{
+
+}
 
 // Error por no tener textura?
 
